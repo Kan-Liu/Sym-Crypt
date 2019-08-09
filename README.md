3c68e7dda7369f632f3b78c7686e902b61ce4d7a
Kan Liu <liukan07@berkeley.edu>
2019-08-08 22:32:15 -0700

    Finish alert

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Sym-Crypt is a React CRA Web App to encrypt/decrypt strings symmetrically if given the same key string.
 
-[Demo App Link](https://kan-liu.github.io/sym-crypt/)
+The app is ready to use and has been deployed onto this [GitHub Page](https://kan-liu.github.io/sym-crypt/). The main encryption/decryption script ([link here](https://github.com/Kan-Liu/sym-crypt/blob/master/src/symCrypt.js)) can also be used separately.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 import * as symCrypt from "./symCrypt";
 import "./App.css";
 
@@ -10,6 +11,10 @@ interface State {
   myEnKey: string;
   toDecrypt: string;
   myDeKey: string;
+  showAlert: boolean;
+  strToShow: string;
+  strHeading: string;
+  alertType: "success" | "danger";
 }
 
 class App extends Component<{}, State> {
@@ -22,13 +27,18 @@ class App extends Component<{}, State> {
     this.handleEncryptKey = this.handleEncryptKey.bind(this);
     this.handleDecryptKey = this.handleDecryptKey.bind(this);
     this.handleClickEncrypt = this.handleClickEncrypt.bind(this);
+    this.handleCloseAlert = this.handleCloseAlert.bind(this);
   }
 
   state: State = {
     toEncrypt: "",
     myEnKey: "",
     toDecrypt: "",
-    myDeKey: ""
+    myDeKey: "",
+    showAlert: false,
+    strToShow: "",
+    strHeading: "",
+    alertType: "success"
   };
 
   handleSubmit(event: any) {
@@ -64,83 +74,121 @@ class App extends Component<{}, State> {
       this.state.toEncrypt,
       this.state.myEnKey
     );
-    alert(`Your encrypted string is: ${encrypted}`);
+
+    this.setState({
+      showAlert: true,
+      strHeading: "Your encrypted string",
+      strToShow: encrypted,
+      alertType: "success"
+    });
   }
 
   handleClickDecrypt(event: any) {
-    const decrypted = symCrypt.symDecrypt(
-      this.state.toDecrypt,
-      this.state.myDeKey
-    );
-    alert(`Your encrypted string is: ${decrypted}`);
+    try {
+      const decrypted = symCrypt.symDecrypt(
+        this.state.toDecrypt,
+        this.state.myDeKey
+      );
+
+      this.setState({
+        showAlert: true,
+        strHeading: "Your decrypted string",
+        strToShow: decrypted,
+        alertType: "success"
+      });
+    } catch (e) {
+      this.setState({
+        showAlert: true,
+        strHeading: "Error",
+        strToShow: "Please eneter a valid encrypted string to decrypt!",
+        alertType: "danger"
+      });
+    }
+  }
+
+  handleCloseAlert() {
+    this.setState({
+      showAlert: false
+    });
   }
 
   render() {
     return (
-      <div>
-        <header className="App-header">
-          Sym-Crypt
-          <br />
-          <header className="App-header3">
-            <Form name="Sym-Crypt Form" onSubmit={this.handleSubmit}>
-              <Form.Group controlId="formToEncrypt">
-                <Form.Label>String to be encrypted</Form.Label>
-                <Form.Control
-                  onChange={this.handleEncrypt}
-                  placeholder="Enter your string to encrypt"
-                  style={{ width: "410px", height: "30px" }}
-                  type="text"
-                />
-              </Form.Group>
+      <div className="App-header">
+        <Alert
+          className="Main-Alert"
+          show={this.state.showAlert}
+          variant={this.state.alertType}
+          onClose={this.handleCloseAlert}
+          dismissible
+        >
+          <Alert.Heading className="Alert-Heading">
+            {this.state.strHeading}
+          </Alert.Heading>
+          <p>{this.state.strToShow}</p>
+        </Alert>
+        Sym-Crypt
+        <br />
+        <header className="App-header3">
+          <Form name="Sym-Crypt Form" onSubmit={this.handleSubmit}>
+            <Form.Group controlId="formToEncrypt">
+              <Form.Label>String to be encrypted</Form.Label>
+              <Form.Control
+                onChange={this.handleEncrypt}
+                placeholder="Enter your string to encrypt"
+                style={{ width: "410px", height: "30px" }}
+                type="text"
+              />
+            </Form.Group>
 
-              <Form.Group controlId="formEncryptKey">
-                <Form.Label>Encryption Key</Form.Label>
-                <Form.Control
-                  onChange={this.handleEncryptKey}
-                  placeholder="Enter your encryption key"
-                  style={{ width: "410px", height: "30px" }}
-                  type="text"
-                />
-              </Form.Group>
+            <Form.Group controlId="formEncryptKey">
+              <Form.Label>Encryption Key</Form.Label>
+              <Form.Control
+                onChange={this.handleEncryptKey}
+                placeholder="Enter your encryption key"
+                style={{ width: "410px", height: "30px" }}
+                type="text"
+              />
+            </Form.Group>
 
-              <Button
-                onClick={this.handleClickEncrypt}
-                style={{ width: "410px" }}
-              >
-                Encrypt
-              </Button>
-              <br />
-              <br />
-              <br />
-              <br />
-              <Form.Group controlId="formToDecrypt">
-                <Form.Label>String to be decrypted</Form.Label>
-                <Form.Control
-                  onChange={this.handleDecrypt}
-                  placeholder="Enter your string to decrypt"
-                  style={{ width: "410px", height: "30px" }}
-                  type="text"
-                />
-              </Form.Group>
+            <Button
+              onClick={this.handleClickEncrypt}
+              style={{ width: "410px" }}
+            >
+              Encrypt
+            </Button>
+            <br />
+            <br />
+            <br />
+            <br />
+            <Form.Group controlId="formToDecrypt">
+              <Form.Label>String to be decrypted</Form.Label>
+              <Form.Control
+                onChange={this.handleDecrypt}
+                placeholder="Enter your string to decrypt"
+                style={{ width: "410px", height: "30px" }}
+                type="text"
+              />
+            </Form.Group>
 
-              <Form.Group controlId="formDecryptKey">
-                <Form.Label>Decryption Key</Form.Label>
-                <Form.Control
-                  onChange={this.handleDecryptKey}
-                  placeholder="Enter your decryption key"
-                  style={{ width: "410px", height: "30px" }}
-                  type="text"
-                />
-              </Form.Group>
+            <Form.Group controlId="formDecryptKey">
+              <Form.Label>Decryption Key</Form.Label>
+              <Form.Control
+                onChange={this.handleDecryptKey}
+                placeholder="Enter your decryption key"
+                style={{ width: "410px", height: "30px" }}
+                type="text"
+              />
+            </Form.Group>
 
-              <Button
-                onClick={this.handleClickDecrypt}
-                style={{ width: "410px" }}
-              >
-                Decrypt
-              </Button>
-            </Form>
-          </header>
+            <Button
+              onClick={this.handleClickDecrypt}
+              style={{ width: "410px" }}
+              variant="info"
+            >
+              Decrypt
+            </Button>
+          </Form>
         </header>
       </div>
     );
